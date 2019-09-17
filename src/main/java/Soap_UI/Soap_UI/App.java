a54c789a785833295c44c7eceaf8cbbb74cf7865
shephalis1234 <54839120+shephalis1234@users.noreply.github.com>
2019-09-17 15:53:00 +0530

    Update App.java

--- src/main/java/Soap_UI/Soap_UI/App.java
+++ src/main/java/Soap_UI/Soap_UI/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello Worldwide" );
+        System.out.println( "Hello Worldwide best" );
     }
 }
